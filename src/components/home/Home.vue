80f51537614012cc4a626dedd265ab0a38968535
魏新秀 <weixinxiu@digilinx.cn>
2018-09-05 15:20:32 +0800

    初始化

---
+++ src/components/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <home-header></home-header>
+</template>
+<script>
+import HomeHeader from "./HomeHeader"
+export default {
+  name:'Home',
+  data(){
+    return {
+
+    }
+  },
+  components : {
+    HomeHeader
+  }  
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/home/HomeHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <div class="back">返回</div>
+    <div>搜索</div>
+    <div>城市</div>
+  </div>
+</template>
+<script>
+export default {
+  name : 'HomeHeader',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import '@/assets/style/scss/base.scss';
+  .header {
+    background-color: $bgcolor;
+    display: flex;
+  }
+</style>
+
